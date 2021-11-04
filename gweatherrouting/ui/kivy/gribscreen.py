142fbfebdeb57818837730e9193b8dff43aa53cd
Davide Gessa (dakk) <gessadavide@gmail.com>
2021-11-04 18:02:10 +0100

    grib remove

--- gweatherrouting/ui/kivy/gribscreen.py
+++ gweatherrouting/ui/kivy/gribscreen.py
@@ -19,30 +19,45 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty, BooleanProperty, NumericProperty, ObjectProperty
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.list import IRightBodyTouch, OneLineIconListItem, TwoLineIconListItem, ThreeLineAvatarIconListItem, OneLineAvatarIconListItem
 from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.uix.boxlayout import MDBoxLayout
+from threading import Thread
+
+class RemoteGribListItem(ThreeLineAvatarIconListItem):
+	callback = ObjectProperty(None)
+	url = StringProperty('')
 
 class LocalGribListItem(ThreeLineAvatarIconListItem):
 	icon = StringProperty("tailwind")
 	active = BooleanProperty(False)
 	gribManager = ObjectProperty()
+	updateCallback = ObjectProperty()
 
 	def setEnabled(self, enabled):
 		self.gribManager.changeState(self.text, enabled)
 
-class RightCheckbox(IRightBodyTouch, MDCheckbox):
+	def remove(self):
+		self.gribManager.remove(self.text)
+		self.updateCallback()
+
+class RightBody(IRightBodyTouch, MDBoxLayout):
 	pass 
 
 class GribScreen(MDScreen):
 	gribManager = None
+	gribDownloadDialog = None
 
 	def updateLocalGribs(self):
 		gribList = self.ids.gribList
+		gribList.clear_widgets()
 		self.gribManager.refreshLocalGribs()
 
 		for x in self.gribManager.localGribs:
 		 	gribList.add_widget(LocalGribListItem(
 				 text=x.name, 
+				 updateCallback=self.updateLocalGribs,
 				 gribManager=self.gribManager,
 				 active=self.gribManager.isEnabled(x.name), 
 				 secondary_text="Start at %s and is valid for %s hours" % (str(x.startTime), str(x.lastForecast)),
@@ -51,3 +66,41 @@ class GribScreen(MDScreen):
 
 	def on_pre_enter(self, *args):
 		self.updateLocalGribs()
+
+	def onSelectRemoteGrib(self, uri):
+		self.gribDownloadDialog.dismiss()
+		self.ids.downloadProgress.value = 0
+
+		def onGribDownloadPercentage(percentage):
+			self.ids.downloadProgress.value = percentage
+
+		def onGribDownloadCompleted(g):
+			self.updateLocalGribs()
+		
+		t = Thread(target=self.gribManager.download, args=(uri, onGribDownloadPercentage, onGribDownloadCompleted,))
+		t.start ()
+
+
+	def openGribDownloadDialog(self):
+		if not self.gribDownloadDialog:
+			items = []
+
+			for x in self.gribManager.getDownloadList():
+				items.append(RemoteGribListItem(
+					text=x[0],
+					secondary_text="Start at %s, %s" % (str(x[3]), str(x[2])),
+					tertiary_text="Centre: %s" % x[1],
+					url=x[4],
+					callback=self.onSelectRemoteGrib
+				))
+			
+			self.gribDownloadDialog = MDDialog(
+				title="Download a GRIB",
+				type="simple",
+				items=items
+			)
+
+		self.gribDownloadDialog.open()
+
+	def openFileChooserDialog(self):
+		pass 
