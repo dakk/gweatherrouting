882304b02913a53b4431ee83fd018c1a81719a81
Davide Gessa (dakk) <gessadavide@gmail.com>
2021-12-08 13:19:34 +0100

    Closes #30

--- gweatherrouting/ui/gtk/mainwindow_time.py
+++ gweatherrouting/ui/gtk/mainwindow_time.py
@@ -22,6 +22,15 @@ gi.require_version('OsmGpsMap', '1.0')
 from gi.repository import Gtk, Gio, GObject, OsmGpsMap, Gdk
 from .timepickerdialog import TimePickerDialog
 
+TIME_UNITS = {
+	'15m': 15,
+	'30m': 30,
+	'1h': 60,
+	'3h': 180,
+	'6h': 360,
+	'12h': 720,
+	'1d': 1440
+}
 
 class MainWindowTime:
 	play = False
@@ -29,8 +38,17 @@ class MainWindowTime:
 	def __init__(self):
 		# self.timeAdjust = self.builder.get_object('time-adjustment')
 		self.timeLabel = self.builder.get_object('time-label')
+		self.timeUnitCombo = self.builder.get_object('time-unit-combo')
 		self.timeControl.connect('time-change', self.onTimeChange)
 		self.onTimeChange(self.timeControl.time)
+
+		self.minutes = 15
+		self.timeUnitCombo.set_active(0)
+
+	def onTimeUnitComboChange(self, widget):
+		u = self.timeUnitCombo.get_active_text()
+		self.minutes = TIME_UNITS[u]
+
 
 	def onTimeChange(self, t):
 		self.timeLabel.set_text("%s" % str(t))
@@ -52,12 +70,12 @@ class MainWindowTime:
 		self.play = False
 
 	def onFowardClick(self, event):
-		self.timeControl.increase(minutes=30)
+		self.timeControl.increase(minutes=self.minutes)
 		self.map.queue_draw ()
 
 	def onBackwardClick(self, event):
 		# if self.timeControl.time > 0:
-		self.timeControl.decrease(minutes=30)
+		self.timeControl.decrease(minutes=self.minutes)
 
 	def onTimeSelect(self, event):
 		tp = TimePickerDialog.create(self.window)
