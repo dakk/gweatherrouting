335ec0927320aec168a45856bfafa42c8dc5993d
scyrock <ciccio.cerrito@gmail.com>
2025-04-15 22:48:40 +0200

    AppImage generation - add libeccodes-dev

--- .github/workflows/script/ubuntu_dep_eccodes_bin.sh
+++ .github/workflows/script/ubuntu_dep_eccodes_bin.sh
@@ -7,13 +7,12 @@ make
 ctest
 sudo make install
 echo "ECCODES_DEFINITION_PATH=/usr/local/share/eccodes/definitions" >> $GITHUB_ENV
-pip install eccodes
 
 
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install libosmgpsmap-1.0-1
+sudo apt-get install libeccodes-dev libosmgpsmap-1.0-1
 sudo apt-get install gdal-bin libgdal-dev
 sudo apt-get install libgtk-4-dev libgtk-4-1 libgirepository1.0-dev
 
