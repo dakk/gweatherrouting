e97bbfab822dad4cdcac669fec655c7c1df0e767
scyrock <ciccio.cerrito@gmail.com>
2025-04-15 21:38:10 +0200

    AppImage generation script 1

---
+++ .github/workflows/script/ubuntu_dep.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install libeccodes0 libeccodes-dev libosmgpsmap-1.0-1
+sudo apt-get install gdal-bin libgdal-dev
+sudo apt-get install libgtk-4-dev libgtk-4-1 libgirepository1.0-dev
+
