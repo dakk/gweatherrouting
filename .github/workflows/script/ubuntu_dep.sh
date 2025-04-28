dab59d4c3b79da17113d8039fce03a01300a9712
Davide Gessa <gessadavide@gmail.com>
2025-04-28 08:54:30 +0200

    create appimage without using fuse

--- .github/workflows/script/ubuntu_dep.sh
+++ .github/workflows/script/ubuntu_dep.sh
@@ -1,4 +1,10 @@
+sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt update
+sudo apt-get install -y libosmgpsmap-1.0-1 libosmgpsmap-1.0-dev
+sudo apt-get install -y gdal-bin libgdal-dev
+sudo apt-get install -y libgtk-4-dev libgtk-4-1 libgirepository1.0-dev
+# sudo apt-get install libeccodes-dev
+
 sudo apt-get install -y libaec-dev cmake
 wget -O eccodes-2.41.0-Source.tar.gz "https://confluence.ecmwf.int/download/attachments/45757960/eccodes-2.41.0-Source.tar.gz?api=v2"
 tar -xzf eccodes-2.41.0-Source.tar.gz
@@ -6,13 +12,6 @@ cd eccodes-2.41.0-Source
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DENABLE_EXAMPLES=OFF -DENABLE_TESTS=OFF -DENABLE_FORTRAN=OFF ..
 make -j`nproc`
-# ctest
 sudo make install
 
-sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
-sudo apt-get update
-sudo apt-get install libosmgpsmap-1.0-1 libosmgpsmap-1.0-dev
-sudo apt-get install gdal-bin libgdal-dev
-sudo apt-get install libgtk-4-dev libgtk-4-1 libgirepository1.0-dev
-# sudo apt-get install libeccodes-dev
 
