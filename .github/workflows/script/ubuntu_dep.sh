0ababab12678401df08d4c07b2fc38934d338f55
Davide Gessa <gessadavide@gmail.com>
2025-04-29 11:23:36 +0200

    fix

--- .github/workflows/script/ubuntu_dep.sh
+++ .github/workflows/script/ubuntu_dep.sh
@@ -1,5 +1,5 @@
 sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt update
+sudo apt-get install -y libgdal34 libgdal-dev gdal-data gdal-plugins 
 sudo apt-get install -y libosmgpsmap-1.0-1 libosmgpsmap-1.0-dev
-sudo apt-get install -y gdal-bin libgdal-dev
 sudo apt-get install -y libgtk-4-dev libgtk-4-1 libgirepository1.0-dev
