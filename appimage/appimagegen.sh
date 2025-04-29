e32b37f1a31b59a0cd2fb5db3acaea0204f9d3f5
Davide Gessa <gessadavide@gmail.com>
2025-04-29 10:07:17 +0200

    remove eccodes from deps

--- appimage/appimagegen.sh
+++ appimage/appimagegen.sh
@@ -41,10 +41,9 @@ cd ..
 
 # 2. Create AppImage Directory Structure
 echo "Creating AppImage directory structure..."
-mkdir -p "$APP_DIR/usr/bin" "$APP_DIR/usr/share/applications" "$APP_DIR/usr/share/icons/hicolor/256x256/apps" "$APP_DIR/usr/local/share/eccodes/" 
+mkdir -p "$APP_DIR/usr/bin" "$APP_DIR/usr/share/applications" "$APP_DIR/usr/share/icons/hicolor/256x256/apps" 
 
 cp -r /usr/share/gdal "$APP_DIR/usr/share/"
-cp -r /usr/local/share/eccodes/definitions "$APP_DIR/usr/local/share/eccodes/"
 cp "gweatherrouting/dist/$APP_NAME" "$APP_DIR/usr/bin/"
 cp "appimage/icon.png" "$APP_DIR/usr/share/icons/hicolor/256x256/apps/"
 cp "appimage/$APP_NAME.desktop" "$APP_DIR/usr/share/applications/"
