fa4530c81388ec3c1eec2864b289c8f2178da27b
Eresia999 <emanuelepistis@gmail.com>
2025-06-04 00:28:16 +0200

    fix: update script paths and simplify directory handling in flatpak workflow

--- .github/workflows/script/flatpak_dep.sh
+++ .github/workflows/script/flatpak_dep.sh
@@ -3,14 +3,13 @@
 set -e
 
 VERSION=$1
-REPO_DIR="$(cd "$(dirname "$0")/.." && pwd)"
-WHEELS_DIR="$REPO_DIR/flatpak/wheels"
+DIR="${PWD}"
 
-mkdir -p "$WHEELS_DIR"
+mkdir -p "$DIR"/flatpak/wheels
 
 flatpak run \
-  --command=sh --devel \
-  --filesystem="$REPO_DIR/flatpak":rw \
-  --share=network \
-  org.gnome.Sdk//"$VERSION" \
-  -c 'pip3 download -r flatpak/requirements.txt -d flatpak/wheels'
+    --command=sh --devel \
+    --filesystem="$DIR"/flatpak:rw \
+    --share=network \
+    org.gnome.Sdk//"$VERSION" \
+	-c 'pip3 download -r requirements.txt -d wheels'
