07ff035104ee4180f1da22ba0de8789b590cb3ad
Eresia999 <emanuelepistis@gmail.com>
2025-06-04 00:20:47 +0200

    fix: update directory paths in flatpak_dep.sh for consistency

--- .github/workflows/script/flatpak_dep.sh
+++ .github/workflows/script/flatpak_dep.sh
@@ -3,13 +3,14 @@
 set -e
 
 VERSION=$1
-DIR="${PWD}"
+REPO_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+WHEELS_DIR="$REPO_DIR/flatpak/wheels"
 
-mkdir -p flatpak/wheels
+mkdir -p "$WHEELS_DIR"
 
 flatpak run \
-	--command=sh --devel \
-	--filesystem="$DIR"/flatpak:rw \
-	--share=network \
-	org.gnome.Sdk//"$VERSION" \
-	-c 'pip3 download -r requirements.txt -d wheels'
+  --command=sh --devel \
+  --filesystem="$REPO_DIR/flatpak":rw \
+  --share=network \
+  org.gnome.Sdk//"$VERSION" \
+  -c 'pip3 download -r flatpak/requirements.txt -d flatpak/wheels'
