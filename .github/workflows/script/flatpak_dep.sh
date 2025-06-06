a1009aca1f997d75e200ba4f5d3b35fe96de5147
Eresia999 <emanuelepistis@gmail.com>
2025-06-06 22:03:32 +0200

    fix: correct filesystem path and restore necessary steps in Flatpak workflow

--- .github/workflows/script/flatpak_dep.sh
+++ .github/workflows/script/flatpak_dep.sh
@@ -5,15 +5,15 @@ set -e
 VERSION=$1
 DIR="${PWD}"
 
-#mkdir -p flatpak/wheels /tmp/flatpak/wheels
-#cp flatpak/requirements.txt /tmp/flatpak/
-#ls /tmp/flatpak
-
-mkdir -p flatpak/wheels
+mkdir -p flatpak/wheels /tmp/flatpak/wheels
+cp flatpak/requirements.txt /tmp/flatpak/
+ls /tmp/flatpak
 
 flatpak run \
     --command=sh --devel \
-    --filesystem="$PWD"/flatpak:rw \
+    --filesystem=/tmp/flatpak:rw \
     --share=network \
     org.gnome.Sdk//"$VERSION" \
 	-c 'pip3 download -r requirements.txt -d wheels'
+
+cp -r /tmp/flatpak/wheels/* flatpak/wheels
