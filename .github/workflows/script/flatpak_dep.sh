749ca3b6ef28a99f9c47584199e0443540b27ff0
Eresia999 <emanuelepistis@gmail.com>
2025-06-06 20:44:53 +0200

    fix: update Flatpak workflow to use GNOME 47 and adjust filesystem paths

--- .github/workflows/script/flatpak_dep.sh
+++ .github/workflows/script/flatpak_dep.sh
@@ -3,14 +3,17 @@
 set -e
 
 VERSION=$1
+DIR="${PWD}"
 
-mkdir -p flatpak/wheels /tmp/flatpak/wheels
-cp flatpak/requirements.txt /tmp/flatpak/
-ls /tmp/flatpak
+#mkdir -p flatpak/wheels /tmp/flatpak/wheels
+#cp flatpak/requirements.txt /tmp/flatpak/
+#ls /tmp/flatpak
+
+mkdir -p flatpak/wheels
 
 flatpak run \
     --command=sh --devel \
-    --filesystem=/tmp/flatpak:rw \
+    --filesystem="$PWD"/flatpak:rw \
     --share=network \
     org.gnome.Sdk//"$VERSION" \
 	-c 'pip3 download -r requirements.txt -d wheels'
