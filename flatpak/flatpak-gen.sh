23d6e7bb9fe60efbe8f08ea8752ec56449a9d1a8
Davide Gessa <gessadavide@gmail.com>
2025-11-26 12:26:07 +0100

    add flatpak files to gitignore, improve flatpak script os detection

--- flatpak/flatpak-gen.sh
+++ flatpak/flatpak-gen.sh
@@ -2,40 +2,64 @@
 
 set -e
 
-DIR="${PWD}"
+# Check for the presence of flatpak and flatpak-builder
+shopt -s nocasematch
 
+DIR="${PWD}"
 os=$(awk -F= '/^ID=/ { gsub(/"/, "", $2); print $2 }' /etc/os-release)
 
-case "$os" in
-    [Aa][Rr][Cc][Hh]*|[Mm][Aa][Nn][Jj][Aa][Rr][Oo]*)
-        sudo pacman -Syu --noconfirm --needed flatpak flatpak-builder
-        ;;
-    [Dd][Ee][Bb][Ii][Aa][Nn]*|[Mm][Ii][Nn][Tt]*|[Uu][Bb][Uu][Nn][Tt][Uu]*)
-        sudo apt update
-        sudo apt install -y flatpak flatpak-builder
-        ;;
-    [Ff][Ee][Dd][Oo][Rr][Aa]*|[Rr][Hh][Ee][Dd][Hh][Aa][Tt]*|[Cc][Ee][Nn][Tt][Oo][Ss]*|[Aa][Ll][Mm][Aa]*|[Rr][Oo][Cc][Kk][Yy]*)
-        sudo dnf check-update
-        sudo dnf install -y flatpak flatpak-builder
-        ;;
-    *)
-        echo "Unsupported OS: $os"
-        exit 1
-        ;;
-esac
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
 
+if command_exists flatpak && command_exists flatpak-builder; then
+    echo "flatpak and flatpak-builder already installed, skipping installation."
+    install_needed=false
+else
+    install_needed=true
+fi
+
+
+# If not present, install it
+if [ "$install_needed" = true ]; then
+    case "$os" in
+        arch*|manjaro*)
+            sudo pacman -Syu --noconfirm --needed flatpak flatpak-builder
+            ;;
+        debian*|mint*|ubuntu*)
+            sudo apt update
+            sudo apt install -y flatpak flatpak-builder
+            ;;
+        fedora*|redhat*|centos*|alma*|rocky*)
+            sudo dnf check-update
+            sudo dnf install -y flatpak flatpak-builder
+            ;;
+        gentoo*)
+            sudo emerge --sync
+            sudo emerge --ask --noreplace flatpak flatpak-builder
+            ;;
+        *)
+            echo "Unsupported OS: $os; please install flatpak flatpak-builder and start the script again"
+            exit 1
+            ;;
+    esac
+fi
+
+shopt -u nocasematch
+
+# Createh the build
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub org.gnome.Platform//46 flathub org.gnome.Sdk//46
 
 if [ ! -d "wheels" ]; then
-	mkdir wheels
+    mkdir wheels
 fi
 
 flatpak run \
-	--command=sh --devel \
-	--filesystem="$DIR":rw \
-	--share=network \
-	org.gnome.Sdk//46 \
-	-c 'pip3 download -r requirements.txt -d wheels'
+    --command=sh --devel \
+    --filesystem="$DIR":rw \
+    --share=network \
+    org.gnome.Sdk//46 \
+    -c 'pip3 download -r requirements.txt -d wheels'
 
 flatpak-builder --user --install --force-clean build-dir org.gweatherrouting.app.yml
