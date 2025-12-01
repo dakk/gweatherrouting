8a88c727b027fe049e3040dc4206b2b5e6190f5c
Emanuele Pistis <emanuelepistis@gmail.com>
2025-12-01 14:32:28 +0100

    Refactor Flatpak build configuration and update Python dependencies

--- flatpak/flatpak-gen.sh
+++ flatpak/flatpak-gen.sh
@@ -25,17 +25,7 @@ case "$os" in
 esac
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub org.gnome.Platform//46 flathub org.gnome.Sdk//46
 
-if [ ! -d "wheels" ]; then
-	mkdir wheels
-fi
+flatpak-builder --install-deps-from=flathub --force-clean build-dir io.github.dakk.gweatherrouting.yml
 
-flatpak run \
-	--command=sh --devel \
-	--filesystem="$DIR":rw \
-	--share=network \
-	org.gnome.Sdk//46 \
-	-c 'pip3 download -r requirements.txt -d wheels'
-
-flatpak-builder --user --install --force-clean build-dir org.gweatherrouting.app.yml
+flatpak-builder --user --install --force-clean build-dir io.github.dakk.gweatherrouting.yml
