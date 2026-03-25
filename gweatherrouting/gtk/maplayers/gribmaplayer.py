85e387fc7410cbedd14b3d7069bc5dba1a2574db
Davide Gessa <gessadavide@gmail.com>
2026-03-25 10:34:39 +0100

    improve wind rendering

--- gweatherrouting/gtk/maplayers/gribmaplayer.py
+++ gweatherrouting/gtk/maplayers/gribmaplayer.py
@@ -16,6 +16,7 @@ For detail about GNU see <http://www.gnu.org/licenses/>.
 
 import math
 
+import cairo
 import gi
 
 gi.require_version("Gtk", "3.0")
@@ -42,30 +43,69 @@ class GribMapLayer(GObject.GObject):
     def on_time_change(self, t):
         pass
 
-    def draw_wind_arrow(self, cr, x, y, wdir, wspeed):
-        wdir = -math.radians(wdir)
+    def draw_wind_barb(self, cr, x, y, wdir, wspeed):
+        """Draw a wind barb in standard meteorological notation.
 
-        a, b, c = wind_color(wspeed)
-        cr.set_source_rgba(a, b, c, self.settings_manager.gribArrowOpacity)
+        The barb points in the direction the wind comes FROM.
+        Short barb = 5 kt, long barb = 10 kt, pennant (flag) = 50 kt.
+        """
+        wspeed_kt = wspeed * 1.94384  # m/s to knots
+        opacity = self.settings_manager.gribArrowOpacity
 
-        length = 15
+        r, g, b = wind_color(wspeed)
+        cr.set_source_rgba(r, g, b, opacity)
 
-        cr.move_to(x, y)
+        # Wind barb points into the wind (direction wind comes FROM)
+        angle = math.radians(wdir)
+        shaft_len = 20
 
-        # cr.line_to (x + (wspeed / 2 * math.sin (wdir)), y + 1 * math.cos (wdir))
-        cr.line_to(x + (length * math.sin(wdir)), y + (length * math.cos(wdir)))
+        cr.save()
+        cr.translate(x, y)
+        cr.rotate(angle)
 
-        cr.line_to(
-            x + (4 * math.sin(wdir - math.radians(30))),
-            y + (4 * math.cos(wdir - math.radians(30))),
-        )
-        cr.move_to(x + (length * math.sin(wdir)), y + (length * math.cos(wdir)))
-        cr.line_to(
-            x + (4 * math.sin(wdir + math.radians(30))),
-            y + (4 * math.cos(wdir + math.radians(30))),
-        )
-
+        # Draw shaft (line pointing up = into the wind)
+        cr.move_to(0, 0)
+        cr.line_to(0, -shaft_len)
         cr.stroke()
+
+        # Draw dot for calm winds
+        if wspeed_kt < 2.5:
+            cr.arc(0, 0, 2, 0, 2 * math.pi)
+            cr.stroke()
+            cr.restore()
+            return
+
+        # Build barbs from tip downward
+        remaining = wspeed_kt
+        barb_y = -shaft_len
+        barb_spacing = 3.5
+        barb_len = 8
+
+        # Pennants (50 kt each)
+        while remaining >= 47.5:
+            cr.move_to(0, barb_y)
+            cr.line_to(barb_len, barb_y)
+            cr.line_to(0, barb_y + barb_spacing)
+            cr.close_path()
+            cr.fill()
+            barb_y += barb_spacing
+            remaining -= 50
+
+        # Long barbs (10 kt each)
+        while remaining >= 7.5:
+            cr.move_to(0, barb_y)
+            cr.line_to(barb_len, barb_y)
+            cr.stroke()
+            barb_y += barb_spacing
+            remaining -= 10
+
+        # Short barb (5 kt)
+        if remaining >= 2.5:
+            cr.move_to(0, barb_y)
+            cr.line_to(barb_len * 0.5, barb_y)
+            cr.stroke()
+
+        cr.restore()
 
     def do_draw(self, gpsmap, cr):
         if not self.visible:
@@ -76,16 +116,11 @@ class GribMapLayer(GObject.GObject):
         p1lat, p1lon = p1.get_degrees()
         p2lat, p2lon = p2.get_degrees()
 
-        # width = float(gpsmap.get_allocated_width())
-        # height = float(gpsmap.get_allocated_height())
-
-        cr.set_line_width(1)
-        cr.set_source_rgb(1, 0, 0)
-        # print (p1lat, p1lon, p2lat, p2lon)
-
+        # Expand bounds by ~1 degree so edge quads still render
+        margin = 1.0
         bounds = (
-            (min(p1lat, p2lat), min(p1lon, p2lon)),
-            (max(p1lat, p2lat), max(p1lon, p2lon)),
+            (min(p1lat, p2lat) - margin, min(p1lon, p2lon) - margin),
+            (max(p1lat, p2lat) + margin, max(p1lon, p2lon) + margin),
         )
         data = self.grib_manager.get_wind_2d(self.time_control.time, bounds)
 
@@ -96,10 +131,11 @@ class GribMapLayer(GObject.GObject):
         if scale < 1:
             scale = 1
 
-        cr.set_line_width(1.5 / (math.ceil(len(data) / 60)))
-        cr.set_line_width(1)
+        # Draw color fill (smooth gradient between grid points)
+        self._draw_color_fill(gpsmap, cr, data, scale)
 
-        # Draw arrows
+        # Draw wind barbs
+        cr.set_line_width(1)
         for x in data[::scale]:
             for y in x[::scale]:
                 if not self.settings_manager.gribArrowOnGround:
@@ -109,46 +145,55 @@ class GribMapLayer(GObject.GObject):
                 xx, yy = gpsmap.convert_geographic_to_screen(
                     MapPoint.new_degrees(y[2][0], y[2][1])
                 )
-                self.draw_wind_arrow(cr, xx, yy, y[0], y[1])
+                self.draw_wind_barb(cr, xx, yy, y[0], y[1])
 
-        # Draw gradients
-        # for i in range(0, len(data) - scale, scale):
-        # 	for j in range(0, len(data[i]) - scale, scale):
-        # 		xx, yy = gpsmap.convert_geographic_to_screen(
-        # 			MapPoint.new_degrees(data[i][j][2][0], data[i][j][2][1])
-        # 		)
+    def _draw_color_fill(self, gpsmap, cr, data, scale):
+        """Draw interpolated color fill between grid points."""
+        opacity = self.settings_manager.gribArrowOpacity * 0.3
+        convert = gpsmap.convert_geographic_to_screen
 
-        # 		try:
-        # 			xx2, yy2 = gpsmap.convert_geographic_to_screen(
-        # 				MapPoint.new_degrees(data[i+scale][j+scale][2][0],
-        #  data[i+scale][j+scale][2][1])
-        # 			)
-        # 		except:
-        # 			continue
+        for i in range(0, len(data) - scale, scale):
+            for j in range(0, len(data[i]) - scale, scale):
+                try:
+                    d_tl = data[i][j]          # top-left
+                    d_tr = data[i][j + scale]  # top-right
+                    d_bl = data[i + scale][j]  # bottom-left
+                    d_br = data[i + scale][j + scale]  # bottom-right
+                except IndexError:
+                    continue
 
-        # 		dx = xx2-xx
-        # 		dy = yy-yy2
+                # Use actual screen coordinates for each corner
+                x_tl, y_tl = convert(MapPoint.new_degrees(d_tl[2][0], d_tl[2][1]))
+                x_tr, y_tr = convert(MapPoint.new_degrees(d_tr[2][0], d_tr[2][1]))
+                x_bl, y_bl = convert(MapPoint.new_degrees(d_bl[2][0], d_bl[2][1]))
+                x_br, y_br = convert(MapPoint.new_degrees(d_br[2][0], d_br[2][1]))
 
-        # 		pat = cairo.MeshPattern()
-        # 		pat.begin_patch()
-        # 		pat.move_to(xx, yy)
-        # 		pat.line_to(dx+xx, yy)
-        # 		pat.line_to(dx+xx, yy+dy)
-        # 		pat.line_to(dx, yy+dy)
+                pat = cairo.MeshPattern()
+                pat.begin_patch()
+                # Path: TL → TR → BR → BL (clockwise)
+                pat.move_to(x_tl, y_tl)
+                pat.line_to(x_tr, y_tr)
+                pat.line_to(x_br, y_br)
+                pat.line_to(x_bl, y_bl)
 
-        # 		a, b, c = wind_color(data[i][j][1])
-        # 		pat.set_corner_color_rgba(0, a, b, c, 0.6)
-        # 		a, b, c = wind_color(data[i][j+1][1])
-        # 		pat.set_corner_color_rgba(1, a, b, c, 0.6)
-        # 		a, b, c = wind_color(data[i+1][j+1][1])
-        # 		pat.set_corner_color_rgba(2, a, b, c, 0.6)
-        # 		a, b, c = wind_color(data[i+1][j][1])
-        # 		pat.set_corner_color_rgba(3, a, b, c, 0.6)
-        # 		pat.end_patch()
+                # Corners match path order: 0=TL, 1=TR, 2=BR, 3=BL
+                r, g, b = wind_color(d_tl[1])
+                pat.set_corner_color_rgba(0, r, g, b, opacity)
+                r, g, b = wind_color(d_tr[1])
+                pat.set_corner_color_rgba(1, r, g, b, opacity)
+                r, g, b = wind_color(d_br[1])
+                pat.set_corner_color_rgba(2, r, g, b, opacity)
+                r, g, b = wind_color(d_bl[1])
+                pat.set_corner_color_rgba(3, r, g, b, opacity)
+                pat.end_patch()
 
-        # 		cr.set_source(pat)
-        # 		cr.rectangle(xx, yy, dx, dy)
-        # 		cr.fill()
+                cr.set_source(pat)
+                cr.move_to(x_tl, y_tl)
+                cr.line_to(x_tr, y_tr)
+                cr.line_to(x_br, y_br)
+                cr.line_to(x_bl, y_bl)
+                cr.close_path()
+                cr.fill()
 
     def do_render(self, gpsmap):
         pass
